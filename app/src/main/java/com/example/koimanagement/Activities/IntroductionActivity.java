6857f30308969d9140737fa68fbe593f18fb315d
trinh <trinhbloc2003@gmail.com>
2024-10-12 01:52:22 +0700

    update Login APi

---
+++ app/src/main/java/com/example/koimanagement/Activities/IntroductionActivity.java
@@ -0,0 +1,18 @@
+package com.example.koimanagement.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.koimanagement.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+        // Các thao tác trên màn hình giới thiệu
+    }
+}
