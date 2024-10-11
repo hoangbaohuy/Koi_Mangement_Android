00dea54252ed4aa7050352402327d0fad7930fd5
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-10-11 09:49:14 +0700

    update product list

--- app/src/main/java/com/example/koimanagement/Models/Product.java
+++ app/src/main/java/com/example/koimanagement/Models/Product.java
@@ -1,10 +1,60 @@
 package com.example.koimanagement.Models;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
     public int ProductId;
     public String ProductName;
     public String ProductDescription;
     public double Price;
     public int StockQuantity;
     // List OrderItem OrderItems
+
+    public Product(int productId, String productName, String productDescription, double price, int stockQuantity) {
+        ProductId = productId;
+        ProductName = productName;
+        ProductDescription = productDescription;
+        Price = price;
+        StockQuantity = stockQuantity;
+    }
+
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(int productId) {
+        ProductId = productId;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getProductDescription() {
+        return ProductDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        ProductDescription = productDescription;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public int getStockQuantity() {
+        return StockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        StockQuantity = stockQuantity;
+    }
 }
