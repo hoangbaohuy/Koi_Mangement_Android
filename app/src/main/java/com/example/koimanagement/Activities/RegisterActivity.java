1efce0eb2c22f6d410302a370cbf951c76bf576f
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-10-12 10:06:36 +0700

    pull main

--- app/src/main/java/com/example/koimanagement/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/koimanagement/Activities/RegisterActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import retrofit2.Callback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +12,16 @@ import com.example.koimanagement.Models.Request.RegisterRequest;
 import com.example.koimanagement.Models.Response.RegisterResponse;
 import com.example.koimanagement.R;
 
+import java.util.concurrent.TimeUnit;
+
 import IService.IAuthApiService;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class RegisterActivity  extends AppCompatActivity {
     private EditText edName , edEmail ,edPassword;
@@ -44,9 +48,8 @@ public class RegisterActivity  extends AppCompatActivity {
 
 
         RegisterRequest request = new RegisterRequest( name ,email,password);
-
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://localhost:7230/Authen/Register")
+                .baseUrl("http://10.0.2.2:7230/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
@@ -62,7 +65,6 @@ public class RegisterActivity  extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Đăng ký thất bại!", Toast.LENGTH_SHORT).show();
                 }
             }
-
             @Override
             public void onFailure(Call<RegisterResponse> call, Throwable t) {
                 Toast.makeText(RegisterActivity.this, "Lỗi: " + t.getMessage(), Toast.LENGTH_SHORT).show();
